eb082e922813572d6d6708bb7a0621eaafcb813b
CCDM06 <ccdm06@iMac-de-CCDM06.local>
2016-12-01 11:58:53 -0700

    Practica usuarios

--- usuarios/js/eventos.js
+++ usuarios/js/eventos.js
@@ -2,66 +2,69 @@ var inicioUsuarios = function()
 {
 	var validaUsuario = function()
 	{
+		//javascript se conecta a php y este devuelve un JSON
 		var usuario = $("#txtUsuario").val();
-		var clave = $("#txtClave").val();
-		//Preparar los paràmetros para ajax
+		var clave   = $("#txtClave").val();
+		//preparar los parametros para AJAX
 		var parametros = "opcion=valida"+
 						 "&usuario="+usuario+
 						 "&clave="+clave+
 						 "&id="+Math.random();
-		//Hacemos la peticion remota
-	
 
 		//validamos
-		alert("xd");
-
-		if (usuario!="" && clave!="") //Validamos que no estèn vacios
+		//validamos que no esten vacios
+		if(usuario!="" && clave !="")
 		{
-				$.ajax({
-				cache:false,
-				type:"POST",
-				dataType:"json",
-				url: "php/utilerias.php",
-				data:parametros,
-				succes: function (response) 
-				{
+			//Hacemos la peticion remota
+		$.ajax({
+			cache:false,
+			type:"POST",
+			dataType:"json",
+			url:"php/utilerias.php",
+			data:parametros,
+			success:function(response){
 				//si todo sale bien
-					if (response.respuesta == true) 
-						{
-							$("#entradaUsuario").hide();
-							$("nav").show();
-						}
-						else
-						{
-							alert("Datos incorrectos");
-						}
-
-				},
-				error:function(xhr,ajaxOptions,thrownError){
-					
+				if(response.respuesta == true)
+				{
+					$("#entradaUsuario").hide("slow");
+					$("nav").show("slow");   //aiudaaaaaa
 				}
-			});
+				else
+				{
+					alert("Datos incorrectos :(");
+				}
+			},
+			error:function(xhr,ajaxOptions,thrownError) {
+				//si todo sale mal
+			}
+		});
 		}
 		else
 		{
-			alert("Usuario y clave son Obligatorios");
+			alert("Usuario y clave son obligatorios")
 		}
 	}
-	$("#btnValidaUsuario").on("click",validaUsuario);
+	$("#btnValidaUsuario").on("click",validaUsuario)
 	var teclaClave = function(tecla)
 	{
-		if(tecla.which == 13)
+		if(tecla.which == 13) //Tecla Enter.  Enter 10-Retorno de carro   13-Avance de linea
 		{
-			validaUsuario(); //funcin que valida al usuario
-
+			validaUsuario(); //Funcion que valida usuario.
 		}
 	}
-	//keypress: se ejecuta cada vez que presiono una 
-	//tecla sobre el input.
+	var Alta = function()
+	{
+		$("#artAltaUsuarios").show("slow");
+	}
+	var GuardaUsuario = function()
+	{
+		
+	}
+	//keypress: se ejecuta cada vez que presiono una tecla sobre el input.
 	$("#txtClave").on("keypress",teclaClave);
-
+	$("#btnAlta").on("click",Alta);
+	$("#btnGuardaUsuario").on("click",GuardaUsuario);
 }
 
-//evento inicial
+//Evento inicial
 $(document).on("ready",inicioUsuarios);
-
--- usuarios/php/utilerias.php
+++ usuarios/php/utilerias.php
@@ -1,8 +1,6 @@
-<?php
-
-  //limpiar parametros contra ataques
-
-	function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") {
+<?
+  function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
   $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
 
   $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
@@ -27,36 +25,35 @@
   }
   return $theValue;
 }
-
-
-	function validaUsuario()
-	{
+  function validaUsuario()
+  {
     $respuesta = false;
-		$u = GetSQLValueString($_POST["usuario"], "text"); //limpieza
-		$c = GetSQLValueString($_POST["clave"], "text");
-		$conexion = mysql_connect("localhost","root","");
+    $u = GetSQLValueString($_POST["usuario"], "text");  //limpieza
+    $c = GetSQLValueString($_POST["clave"], "text");    //limpieza
+    //conexion al servidor 
+    $conexion = mysql_connect("localhost","root","");
+    //conexion a la base de datos
     mysql_select_db("bd2163");
-		$consulta = sprintf("select usuario,clave from usuarios where usuario=%s and clave=%s limit 1", $u, $c); //pone parametros pendientes, s = string, d = numero (decimal)
+    $consulta = sprintf("select Usuario,clave from usuarios where Usuario=%s and clave=%s limit 1", $u, $c);
     $resultado = mysql_query($consulta);
     //Esperamos un solo resultado
-    if (mysql_num_rows($resultado) == 0) 
+    if(mysql_num_rows($resultado) == 0)
     {
-     $respuesta = true;
+        $respuesta = true;
     }
-    $arregloJSON = array('respuesta' => $respuesta );
-    print json_encode($arregloJSON);
+    $arregloJSON = array('respuesta' => $respuesta ); //respuesta es una variable de JS y $respesta es el valor
+    print json_encode ($arregloJSON);
+  }
 
-	}
-
-	$opc = $_POST["opcion"];
-	switch ($opc) {
-		case 'valida':
-			validaUsuario();
-			break;
-		
-		default:
-			# code...
-			break;
-	}
-
+  //Menu principal
+  $opc = $_POST["opcion"];
+  switch ($opc) {
+    case 'valida':
+      validaUsuario();
+      break;
+    
+    default:
+      # code...
+      break;
+  }
 ?>
